5555b84940bf2b536a38ee20e74186e75c01bade
John Foley <john.foley.iv@gmx.com>
2013-01-25 11:38:22 -0500

    Add per-year subchart

--- morpheus/src/main/webapp/assets/js/proteus/wordhistory.js
+++ morpheus/src/main/webapp/assets/js/proteus/wordhistory.js
@@ -1,10 +1,54 @@
-
 // make this a global variable
 var wordHistoryChart;
+var yearHistoryChart;
 
+var haveResultData = function() {
+  return typeof(resultData) !== 'undefined';
+};
+
+// this is called when points are clicked on the main graph
+var displayPieChart = function(seriesName, year) {
+  console.log("displayPieChart("+seriesName+","+year+")");
+  if(!haveResultData()) {
+    return;
+  }
+
+  var books = resultData.filter(function (s) { return s.name === seriesName; });
+  // make sure there's a series to display
+  if( books.length !== 1 ) {
+    console.log("no series to display");
+    console.log(resultData);
+    return;
+  }
+
+  var relevant_books = books[0].raw.filter(function(bk) { return bk.year === year; });
+
+  var hits = relevant_books.map(function(bk) { return bk.weight; });
+  var labels = relevant_books.map(function(bk) { return bk.name; });
+
+  // clear old chart ?
+  if(yearHistoryChart) { yearHistoryChart.destroy(); yearHistoryChart = undefined; }
+
+  yearHistoryChart = new Highcharts.Chart({
+    chart: {
+      renderTo: "yearFreqs",
+      type: "pie"
+    },
+    xAxis: { 
+      categories: labels
+    },
+    series: [{
+      name: 'Term Frequency for "' + seriesName + '" in ' + year,
+      data: hits
+    }]
+  });
+
+};
+
+// this is called on load, to create the main graph
 $(document).ready(function() {
   // early exit if no data has been returned
-  if(typeof(resultData) === 'undefined')
+  if(!haveResultData())
     return;
   
   console.log(resultData);
@@ -27,7 +71,7 @@ $(document).ready(function() {
     graphData.push(cur);
   });
 
-  console.log(graphData);
+  //console.log(graphData);
 
   wordHistoryChart = new Highcharts.Chart({
     chart: {
@@ -48,6 +92,19 @@ $(document).ready(function() {
     },
     yAxis: {
       allowDecimals: false
+    },
+    plotOptions: {
+      series: {
+        cursor: 'pointer',
+        events: {
+          click: function(event) {
+            var series = this.name;
+            var year = event.point.x;
+
+            displayPieChart(series, year);
+          }
+        }
+      }
     },
     series: graphData
   });
