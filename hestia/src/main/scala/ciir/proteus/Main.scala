1e7cca9f14b8f2fa7b489f2a11286f4f473c454b
John Foley <john.foley.iv@gmx.com>
2013-02-26 15:05:48 -0500

    Setting up for offline tools, parsing Aura config

--- hestia/src/main/scala/ciir/proteus/Main.scala
+++ hestia/src/main/scala/ciir/proteus/Main.scala
@@ -1,8 +1,32 @@
-package ciir.ttools;
+package ciir.proteus;
+
+import java.io.File
+import org.lemurproject.galago.tupleflow.Parameters
 
 object Main {
-  def main(args: Array[String]) {
-    println("Hello World!");
+  def main(argv: Array[String]) {
+    val (files, args) = argv.partition { arg => new File(arg).exists() }
+
+    val parameters = new Parameters()
+    
+    // load all json files given
+    files.map(f => {
+      parameters.copyFrom(Parameters.parse(new File(f)))
+    })
+
+    // read parameters from arguments next
+    parameters.copyFrom(new Parameters(args));
+
+    parameters.getString("adapter") match {
+      case "galago" => {
+        println("Using Galago backend")
+      }
+      case x => {
+        println("Unknown adapter given to hestia:ciir.proteus.Main `"+x+"'")
+        sys.exit()
+      }
+    }
+
   }
 }
 
