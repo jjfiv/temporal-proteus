febe27d74c43d84cec0f72582bf261228f598517
John Foley <john.foley.iv@gmx.com>
2013-02-27 16:44:44 -0500

    Modified Soundex

--- hestia/src/main/scala/ciir/proteus/Hestia.scala
+++ hestia/src/main/scala/ciir/proteus/Hestia.scala
@@ -55,34 +55,8 @@ object SoundexAlgorithm {
 
   private def normalize(ch: Char): Char = { if(ch.isLetter) ch else 'a' }
 
-  def apply(in: String) = {
-    assert(!in.isEmpty)
-    var sb = new StringBuilder
-    sb += in.head.toLower
-
-    var lastNum = getNum(normalize(sb.last))
-    var lastVowel = false
-    var lastHW = false
-    
-    in.tail.foreach(ch => {
-      if(sb.length < 4) {
-        val letter = normalize(ch)
-        val num = getNum(letter)
-        val prevNumSame = lastNum == num
-
-        //println("proc '"+letter+" num="+num+"', hw="+lastHW+" prev="+lastNum)
-        if (num == '0') {
-          // drop this character
-        } else if (prevNumSame && (lastHW || !lastVowel)) {
-          // drop this character
-        } else {
-          sb += num
-          lastNum = num
-        }
-        lastVowel = isVowel(letter)
-        lastHW = (letter == 'h' || letter == 'w')
-      }
-    })
+  def regular(in: String) = {
+    var sb = new StringBuilder(long(in));
     
     if(sb.length > 4) {
       sb.setLength(4)
@@ -95,20 +69,51 @@ object SoundexAlgorithm {
     sb.result
   }
 
+  def long(in: String) = {
+    assert(!in.isEmpty)
+    var sb = new StringBuilder
+    sb += in.head.toLower
+
+    var lastNum = getNum(normalize(sb.last))
+    var lastVowel = false
+    var lastHW = false
+    
+    in.tail.foreach(ch => {
+      val letter = normalize(ch)
+      val num = getNum(letter)
+      val prevNumSame = lastNum == num
+
+      //println("proc '"+letter+" num="+num+"', hw="+lastHW+" prev="+lastNum)
+      if (num == '0') {
+        // drop this character
+      } else if (prevNumSame && (lastHW || !lastVowel)) {
+        // drop this character
+      } else {
+        sb += num
+        lastNum = num
+      }
+      lastVowel = isVowel(letter)
+      lastHW = (letter == 'h' || letter == 'w')
+    })
+    
+    sb.result
+  }
+
   def test() {
     // algorithm, examples taken from Wikipedia
-    assert(apply("Robert") == "r163")
-    assert(apply("Robert") == apply("Rupert"))
-    assert(apply("Ashcraft") == "a261")
-    assert(apply("Tymczak") == "t522")
-    assert(apply("Pfister") == "p236")
+    assert(regular("Robert") == "r163")
+    assert(regular("Robert") == regular("Rupert"))
+    assert(regular("Ashcraft") == "a261")
+    assert(regular("Tymczak") == "t522")
+    assert(regular("Pfister") == "p236")
   }
 }
 
 // use the date data to read in the set of terms in the corpus
 class CorpusWordSet(parameters: Parameters) {
   import org.lemurproject.galago.core.index.disk.DiskBTreeReader
-  import gnu.trove.map.hash.TObjectIntHashMap
+  import org.lemurproject.galago.tupleflow.VByteInput;
+  import gnu.trove.map.hash._
 
   val path = parameters.getString("dateDirectory")
   //val path = parameters.getMap("handlers").getMap("collection").getString("index")
@@ -117,6 +122,8 @@ class CorpusWordSet(parameters: Parameters) {
   val index = new DiskBTreeReader(dfile)
   printf("Opening word frequency index at %s\n", dfile.getCanonicalPath)
 
+  val bookStats = false
+
   def process() {
     var numTerms = 0
     var uniqTerms = 0
@@ -124,7 +131,10 @@ class CorpusWordSet(parameters: Parameters) {
     var singleTerms = 0
     val iter = index.getIterator()
 
-    val smap = new TObjectIntHashMap[String]()
+    val srMap = new TObjectIntHashMap[String]()
+    val slMap = new TObjectIntHashMap[String]()
+    val wordsPerDateMap = new TIntIntHashMap()
+    val booksPerDateMap = new TIntIntHashMap()
     
     var i=0
     var soundexSamples = 0
@@ -136,12 +146,24 @@ class CorpusWordSet(parameters: Parameters) {
       // soundexify everything that passes through
       val s = new String(bytesOfKey)
       if(!s.isEmpty) {
-        smap.adjustOrPutValue(SoundexAlgorithm(s), 1, 1)
+        srMap.adjustOrPutValue(SoundexAlgorithm.regular(s), 1, 1)
+        slMap.adjustOrPutValue(SoundexAlgorithm.long(s), 1, 1)
         soundexSamples += 1
       }
 
       val stream = iter.getSubValueStream(0, iter.getValueLength)
       val count = stream.readInt()
+
+      if(bookStats) {
+        val vStream = new VByteInput(stream)
+        for(i <- 0 until count) {
+          val date = vStream.readInt
+          val weight = vStream.readInt
+
+          wordsPerDateMap.adjustOrPutValue(date, weight, weight)
+          booksPerDateMap.adjustOrPutValue(date, 1, 1)
+        }
+      }
 
       iter.nextKey();
       
@@ -156,13 +178,31 @@ class CorpusWordSet(parameters: Parameters) {
       }
     }
 
+    def fraction(a: Int, b: Int): Double = a.toDouble/b.toDouble
+
     printf("Unique Terms: %d\n",uniqTerms);
     printf("Total Terms: %d\n",numTerms);
     printf("Num Terms with Spaces: %d\n", numTermsWithSpaces)
     printf("Terms with tf=1: %d\n", singleTerms)
-    printf("Fraction Single Terms: %.3f\n", singleTerms.toDouble/uniqTerms.toDouble)
+    printf("Fraction Single Terms: %.3f\n", fraction(singleTerms,uniqTerms))
+    
     printf("Soundex Samples: %d\n", soundexSamples)
-    printf("Soundex Equivalence Classes: %d\n", smap.size())
+    printf("Soundex Equivalence Classes. Regular: %d Long: %d\n",
+      srMap.size(), slMap.size())
+    printf("Mean Words Per Class: Regular: %.3f Long: %.3f\n",
+      fraction(soundexSamples, srMap.size()),
+      fraction(soundexSamples, slMap.size()))
+
+    if(bookStats) {
+      val uniqYears = booksPerDateMap.size()
+
+      printf("Unique Years: %d\n", uniqYears)
+
+      printf("Date, Books, Words\n")
+      for(year <- booksPerDateMap.keys()) {
+        printf("%4d %8d %8d\n", year, booksPerDateMap.get(year), wordsPerDateMap.get(year))
+    }
+    }
   }
 
 }
